a4c59827e9e416644e39223574a772cdd76de54a
Carlo Nicora <carlo.nicora@gmail.com>
2020-12-29 13:19:47 +0100

    updated to latest minimalism

--- src/Twig.php
+++ src/Twig.php
@@ -4,6 +4,7 @@ namespace CarloNicora\Minimalism\Services\Twig;
 use CarloNicora\JsonApi\Document;
 use CarloNicora\Minimalism\Interfaces\ServiceInterface;
 use CarloNicora\Minimalism\Interfaces\TransformerInterface;
+use CarloNicora\Minimalism\Services\Path;
 use Exception;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
@@ -11,18 +12,20 @@ use Twig\Loader\FilesystemLoader;
 class Twig implements ServiceInterface, TransformerInterface
 {
     /**
-     *
+     * Twig constructor.
+     * @param Path $path
      */
-    public function initialise(): void
-    {
-    }
+    public function __construct(private Path $path) {}
 
     /**
      *
      */
-    public function destroy(): void
-    {
-    }
+    public function initialise(): void {}
+
+    /**
+     *
+     */
+    public function destroy(): void {}
 
     /**
      * @param Document $document
@@ -32,10 +35,22 @@ class Twig implements ServiceInterface, TransformerInterface
      */
     public function transform(Document $document, string $viewFile): string
     {
-        $twigLoader = new FilesystemLoader();
+        $paths = [];
+
+        if (file_exists(($defaultDirectory = $this->path->getRoot() . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Views'))){
+            $paths[] = $defaultDirectory;
+        }
+
+        /*
+        foreach ($this->services->paths()->getServicesViewsDirectories() as $additionalPaths) {
+            $paths[] = $additionalPaths;
+        }
+        */
+
+        $twigLoader = new FilesystemLoader($paths);
         $environment = new Environment($twigLoader);
 
-        return $environment->render($viewFile, $document->prepare());
+        return $environment->render($viewFile . '.twig', $document->prepare());
     }
 
     /**
